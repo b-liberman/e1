aa340a263ecc6b7615516b13393dac92fb0f7968
Boris Liberman <bliberman@it-economics.de>
2018-02-06 17:32:06 +0100

    master: tenth commit

--- sc/src/main/java/boris/test/sc/sc/ScTest.java
+++ sc/src/main/java/boris/test/sc/sc/ScTest.java
@@ -9,6 +9,6 @@ public class ScTest {
 	public void ttt() {
 		System.out.println("xxyyzz");
 		System.out.println("add");
-		System.out.println("this is from b2" + "this is another one");
+		System.out.println("this is from b2" + "this is another oneCC");
 	}
 }
